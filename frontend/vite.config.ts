3a0c2552a5b9e275b8135988eba596d90cae70c3
cuboza <cuboza@gmail.com>
2025-11-26 23:13:21 -0300

    feat: add vite proxy for local development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
     'process.env': process.env,
     global: 'window',
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
